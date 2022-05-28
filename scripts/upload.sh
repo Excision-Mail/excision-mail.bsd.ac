8307493ea93675d8cb928df55e0a20e4e95949e4
Aisha Tammy <floss@bsd.ac>
2022-05-28 21:52:20 +0000

    update for Sat May 28 21:52:19 -00 2022
    Signed-off-by: Aisha Tammy <floss@bsd.ac>

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,1 +1,20 @@
 #!/bin/sh
+
+hugo -D
+
+cdate="$(date)"
+
+cd excision-mail.github.io
+git add .
+git commit -m "update for ${cdate}"
+git push
+cd ..
+
+git add .
+git commit -m "update for ${cdate}"
+git push
+
+ssh excision.bsd.ac <<EOF
+cd excision
+git pull
+EOF
